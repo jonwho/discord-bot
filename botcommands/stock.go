69621e0f9cfd3596b3f1f6de6fd7578259eca40d
Jon Ho <jonwho.contact@gmail.com>
2020-07-13 19:47:27 -0700

    ohnice channelsssssssssssssssss

--- botcommands/stock.go
+++ botcommands/stock.go
@@ -36,8 +36,6 @@ func NewStock(iexToken, alpacaID, alpacaKey string) *Stock {
 
 // Execute fetches quote for the ticker symbol
 func (s *Stock) Execute(ctx context.Context, rw io.ReadWriter) error {
-	dataURL := "https://data.alpaca.markets/v1/bars/day"
-
 	buf, err := ioutil.ReadAll(rw)
 	if err != nil {
 		return err
@@ -50,67 +48,31 @@ func (s *Stock) Execute(ctx context.Context, rw io.ReadWriter) error {
 
 	go func() {
 		// get quote from IEX
-		quote, err := s.iexClient.Quote(ticker, &iex.QuoteQueryParams{DisplayPercent: true})
-		if err != nil {
-			ch <- err.Error()
+		quoteCh := s.getQuote(ticker)
+		// get bar from Alpaca
+		barCh := s.getBar(ticker)
+
+		quoteOrErr := <-quoteCh
+		barOrErr := <-barCh
+
+		// err check for quote
+		if quoteOrErr.err != nil {
+			ch <- quoteOrErr.err.Error()
 			return
 		}
-		if quote == nil {
+		if quoteOrErr.quote == nil {
 			msg := fmt.Sprintf("nil quote from ticker: %s", ticker)
 			ch <- msg
 			return
 		}
 
-		// get bar from Alpaca
-		req, err := http.NewRequest(http.MethodGet, dataURL, nil)
-		if err != nil {
-			ch <- err.Error()
-			return
-		}
-		req.Header.Set("APCA-API-KEY-ID", s.alpacaID)
-		req.Header.Set("APCA-API-SECRET-KEY", s.alpacaKey)
-		q := req.URL.Query()
-		q.Add("limit", "1")
-		q.Add("symbols", ticker)
-		req.URL.RawQuery = q.Encode()
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			ch <- err.Error()
-			return
-		}
-		defer resp.Body.Close()
-
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			ch <- err.Error()
-			return
-		}
-		data := map[string][]struct {
-			Time   int64   `json:"t"`
-			Open   float32 `json:"o"`
-			High   float32 `json:"h"`
-			Low    float32 `json:"l"`
-			Close  float32 `json:"c"`
-			Volume int32   `json:"v"`
-		}{}
-		err = json.Unmarshal(bodyBytes, &data)
-		if err != nil {
-			ch <- err.Error()
-			return
-		}
-		if len(data[ticker]) == 0 {
-			ch <- errors.New("Alpaca API no data found for " + ticker).Error()
+		// err check for bar
+		if barOrErr.err != nil {
+			ch <- barOrErr.err.Error()
 			return
 		}
 
-		bar := data[ticker][len(data[ticker])-1]
-		if err != nil {
-			ch <- err.Error()
-			return
-		}
-
-		quoteStr := util.FormatStock(quote, bar)
+		quoteStr := util.FormatStock(quoteOrErr.quote, *barOrErr.bar)
 		ch <- quoteStr
 	}()
 
@@ -123,4 +85,107 @@ func (s *Stock) Execute(ctx context.Context, rw io.ReadWriter) error {
 	}
 
 	return nil
+}
+
+// Generator pattern - return a channel that another goroutine will OBSERVE
+// use channels to communicate between 2 goroutines
+func (s *Stock) getQuote(ticker string) <-chan struct {
+	quote *iex.Quote
+	err   error
+} {
+	ch := make(chan struct {
+		quote *iex.Quote
+		err   error
+	}, 1)
+
+	go func() {
+		quote, err := s.iexClient.Quote(ticker, &iex.QuoteQueryParams{DisplayPercent: true})
+		ch <- struct {
+			quote *iex.Quote
+			err   error
+		}{quote: quote, err: err}
+	}()
+
+	return ch
+}
+
+type alpacaBar struct {
+	Time   int64   `json:"t"`
+	Open   float32 `json:"o"`
+	High   float32 `json:"h"`
+	Low    float32 `json:"l"`
+	Close  float32 `json:"c"`
+	Volume int32   `json:"v"`
+}
+
+func (s *Stock) getBar(ticker string) <-chan struct {
+	bar *alpacaBar
+	err error
+} {
+	ch := make(chan struct {
+		bar *alpacaBar
+		err error
+	}, 1)
+	dataURL := "https://data.alpaca.markets/v1/bars/day"
+
+	go func() {
+		req, err := http.NewRequest(http.MethodGet, dataURL, nil)
+		if err != nil {
+			ch <- struct {
+				bar *alpacaBar
+				err error
+			}{nil, err}
+			return
+		}
+		req.Header.Set("APCA-API-KEY-ID", s.alpacaID)
+		req.Header.Set("APCA-API-SECRET-KEY", s.alpacaKey)
+		q := req.URL.Query()
+		q.Add("limit", "1")
+		q.Add("symbols", ticker)
+		req.URL.RawQuery = q.Encode()
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			ch <- struct {
+				bar *alpacaBar
+				err error
+			}{nil, err}
+			return
+		}
+		defer resp.Body.Close()
+
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			ch <- struct {
+				bar *alpacaBar
+				err error
+			}{nil, err}
+			return
+		}
+		data := map[string][]alpacaBar{}
+		err = json.Unmarshal(bodyBytes, &data)
+		if err != nil {
+			ch <- struct {
+				bar *alpacaBar
+				err error
+			}{nil, err}
+			return
+		}
+		if len(data[ticker]) == 0 {
+			err = errors.New("Alpaca API no data found for " + ticker)
+			ch <- struct {
+				bar *alpacaBar
+				err error
+			}{nil, err}
+			return
+		}
+
+		bar := data[ticker][len(data[ticker])-1]
+		ch <- struct {
+			bar *alpacaBar
+			err error
+		}{&bar, err}
+	}()
+
+	return ch
 }
